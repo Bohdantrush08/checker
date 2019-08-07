782f662a8fcec1d828919ae723022499d44a4b47
Bohdantrush08 <32901504+Bohdantrush08@users.noreply.github.com>
2019-08-07 15:18:15 +0300

    Initial commit

---
+++ src/main/java/com/taskspring/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.taskspring.repos;
+
+import com.taskspring.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
